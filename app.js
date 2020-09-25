77022cbd7e436799e731ffdb678363edde5f9ab5
Shreyansh Shrey <shreyansh.shrey1999@gmail.com>
2020-09-26 00:32:45 +0530

    Level 2 - Encryption

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
+const encrypt = require("mongoose-encryption");
 
 const app = express();
 
@@ -13,10 +14,14 @@ app.use(express.static("public"));
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended : true}));
 
-const userSchema = {
+const userSchema = new mongoose.Schema({
   email : String,
   password : String
-};
+});
+
+const secret = "ThisIsUsedAsAnEncriptionKey.";
+userSchema.plugin(encrypt, { secret: secret, encryptedFields: ['password'] });
+
 
 const User = new mongoose.model("User", userSchema);
 

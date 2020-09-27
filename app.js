82ec763d7401c8e93bb5971c5d03bfc8434af676
Shreyansh Shrey <shreyansh.shrey1999@gmail.com>
2020-09-27 15:32:01 +0530

    Level 3 - Hashing with md5

--- app.js
+++ app.js
@@ -5,11 +5,10 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
+const md5 = require('md5');
 
 const app = express();
 
-console.log(process.env.SECRET);
 
 mongoose.connect("mongodb://localhost:27017/userDB", { useNewUrlParser: true, useUnifiedTopology: true });
 
@@ -23,7 +22,6 @@ const userSchema = new mongoose.Schema({
 });
 
 
-userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ['password'] });
 
 
 const User = new mongoose.model("User", userSchema);
@@ -47,7 +45,7 @@ app.post("/register", function(req, res){
 
   const user = new User({
     email : req.body.username,
-    password : req.body.password
+    password : md5(req.body.password)
   });
 
   user.save(function(err){
@@ -66,7 +64,7 @@ app.post("/login", function(req, res){
       if(!foundUser)
         res.send("Username not found!");
       else{
-        if(foundUser.password === req.body.password)
+        if(foundUser.password === md5(req.body.password))
           res.render("secrets");
         else
           res.send("Password incorrect !");
